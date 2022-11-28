5e648fe6bb417ae3163adbd5ea46c9713db1c9de
JunonPengi <114993029+JunonPengi@users.noreply.github.com>
2022-11-28 12:19:51 +0100

    ajout du chrono

--- pageAccueil.js
+++ pageAccueil.js
@@ -65,3 +65,67 @@ console.log("myJson2", myJSON2)
         };
     })       
 */
+
+let bodyEl = document.getElementsByTagName("body")[0];
+let titleEl = document.getElementById("title");
+let minuteurEl = document.getElementById("minuteur");
+let messageEl = document.getElementById("message");
+let joursEl = document.getElementById("j");
+let heuresEl = document.getElementById("h");
+let minutesEl = document.getElementById("m");
+let secondesEl = document.getElementById("s");
+
+// get UTC offset once
+let now = new Date();
+const dateOffsetInMinutes = now.getTimezoneOffset();
+
+const unJourEnMs = 1000 * 60 * 60 * 24;
+const uneHeureEnMs = 1000 * 60 * 60;
+const uneMinuteEnMs = 1000 * 60;
+
+// const newYear = Date.now() - (dateOffsetInMinutes * uneMinuteEnMs) + 2000;
+const newEnd = new Date("Dec 14 2022");
+
+const getCountdown = () => {
+    let nowDate = Date.now();
+
+    let tempsRestantEnMs =
+        newEnd - nowDate + dateOffsetInMinutes * uneMinuteEnMs;
+
+    console.log(tempsRestantEnMs);
+
+    // jours
+    let nbJours = Math.floor(tempsRestantEnMs / unJourEnMs);
+
+    // heures
+    let resteTempsSansJoursMs = tempsRestantEnMs - nbJours * unJourEnMs;
+    let nbHeures = Math.floor(resteTempsSansJoursMs / uneHeureEnMs);
+
+    // minutes
+    let resteTempsSansHeuresMs =
+        resteTempsSansJoursMs - nbHeures * uneHeureEnMs;
+    let nbMinutes = Math.floor(resteTempsSansHeuresMs / uneMinuteEnMs);
+
+    // secondes
+    let resteTempsSansMinutesMs =
+        resteTempsSansHeuresMs - nbMinutes * uneMinuteEnMs;
+    let nbSecondes = Math.floor(resteTempsSansMinutesMs / 1000);
+
+    joursEl.textContent = nbJours;
+    heuresEl.textContent = nbHeures;
+    minutesEl.textContent = nbMinutes;
+    secondesEl.textContent = nbSecondes;
+
+    if (tempsRestantEnMs <= 0) {
+        joursEl.textContent = 19;
+        heuresEl.textContent = 9;
+        minutesEl.textContent = 33;
+        secondesEl.textContent = 0;
+        titleEl.innerHTML = "Avatar 2";
+    }
+};
+
+let countDownInterval = setInterval(getCountdown, 1000);
+
+// init
+getCountdown();
